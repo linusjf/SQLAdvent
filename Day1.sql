ce5816f09ddbc5901d065c1a7effca66475efb08
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-04 16:13:00 +0530

    fix: update sql statements to use autoincrement

--- Day1.sql
+++ Day1.sql
@@ -1,7 +1,7 @@
 drop table if exists rentals;
 -- Create the rentals table
 CREATE TABLE if not exists rentals (
-  rental_id INTEGER PRIMARY KEY,
+  rental_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   customer_name TEXT NOT NULL,
   activity TEXT NOT NULL,
   rental_date DATE NOT NULL
@@ -10,11 +10,13 @@ CREATE TABLE if not exists rentals (
 CREATE INDEX if not exists idx_customer_name ON rentals (customer_name);
 
 -- Insert row records into the rentals table
-INSERT INTO rentals (rental_id, customer_name, activity, rental_date)
+INSERT INTO rentals (customer_name, activity, rental_date)
 VALUES
-  (1, 'Emily', 'Skiing', '2024-01-01'),
-  (2, 'Michael', 'Snowboarding', '2024-01-02'),
-  (3, 'Emily', 'Snowboarding', '2024-01-03'),
-  (4, 'Sarah', 'Skiing', '2024-01-01'),
-  (5, 'Michael', 'Skiing', '2024-01-02'),
-  (6, 'Michael', 'Snowtubing', '2024-01-02');
+  ('Emily', 'Skiing', '2024-01-01'),
+  ('Michael', 'Snowboarding', '2024-01-02'),
+  ('Emily', 'Snowboarding', '2024-01-03'),
+  ('Sarah', 'Skiing', '2024-01-01'),
+  ('Michael', 'Skiing', '2024-01-02'),
+  ('Michael', 'Snowtubing', '2024-01-02');
+
+SELECT * FROM rentals;
--- Day3.sql
+++ Day3.sql
@@ -3,19 +3,21 @@ DROP TABLE IF EXISTS candy_nutrition;
 
 -- Create the candy_nutrition table
 CREATE TABLE IF NOT EXISTS candy_nutrition (
-  candy_id INTEGER PRIMARY KEY,
+  candy_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL ,
   candy_name TEXT NOT NULL,
   calories INTEGER NOT NULL,
   candy_category TEXT NOT NULL
 );
 
 -- Insert row records into the candy_nutrition table
-INSERT INTO candy_nutrition (candy_id, candy_name, calories, candy_category)
+INSERT INTO candy_nutrition (candy_name, calories, candy_category)
 VALUES
-  (1, 'Candy Cane', 200, 'Sweets'),
-  (2, 'Chocolate Bar', 250, 'Chocolate'),
-  (3, 'Gingerbread Cookie', 150, 'Baked Goods'),
-  (4, 'Lollipop', 100, 'Sweets'),
-  (5, 'Dark Chocolate Truffle', 180, 'Chocolate'),
-  (6, 'Marshmallow', 900, 'Sweets'),
-  (7, 'Sugar Cookie', 140, 'Baked Goods');
+  ('Candy Cane', 200, 'Sweets'),
+  ('Chocolate Bar', 250, 'Chocolate'),
+  ('Gingerbread Cookie', 150, 'Baked Goods'),
+  ('Lollipop', 100, 'Sweets'),
+  ('Dark Chocolate Truffle', 180, 'Chocolate'),
+  ('Marshmallow', 900, 'Sweets'),
+  ('Sugar Cookie', 140, 'Baked Goods');
+
+SELECT * FROM candy_nutrition;
