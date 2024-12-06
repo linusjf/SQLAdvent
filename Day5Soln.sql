221f5eed65444ba04bf93303d5cd09b7602b58b4
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-06 10:10:11 +0530

    feat: update Day 5 solution to filter for Christmas Day

--- Day5Soln.sql
+++ Day5Soln.sql
@@ -1,1 +1,1 @@
-select beach_name, country from beach_temperature_predictions where expected_temperature_c > 30;
+select beach_name, country from beach_temperature_predictions where expected_temperature_c > 30 and date='2024-12-25';
