93990b5fa2137bf45f808f849c18651ce5b066d1
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-07 08:18:44 +0530

    fix: update Day 6 solution to filter for year

--- Day6Soln.sql
+++ Day6Soln.sql
@@ -1,1 +1,2 @@
-select a.bear_id as bear_id, bear_name, sum(distance_km) as total_distance_travelled from polar_bears a join tracking b on a.bear_id = b.bear_id and strftime('%m', date) = '12' group by a.bear_id, a.bear_name order by total_distance_travelled desc limit 3;
+select a.bear_id as bear_id, bear_name, sum(distance_km) as total_distance_travelled from polar_bears a join tracking b on a.bear_id = b.bear_id and strftime('%m', date) = '12'
+and strftime('%Y', date) = '2024' group by a.bear_id, a.bear_name order by total_distance_travelled desc limit 3;
