e04eea8623cbe34fe65def33542ae48ada73248d
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-10 12:24:56 +0530

    feat: update Day 9 solution

--- Day9Soln.sql
+++ Day9Soln.sql
@@ -25,3 +25,15 @@ FROM (
 )
 WHERE
   row_num <= 2;
+with dishes as (SELECT
+    dish_name,
+    event_name,
+    round(calories / weight_g,2) AS calorie_density,
+    ROW_NUMBER() OVER (PARTITION BY menu.event_id ORDER BY round(calories / weight_g,2) DESC) AS row_num
+  FROM
+    menu
+  JOIN
+    events ON menu.event_id = events.event_id
+)
+select dish_name, event_name, calorie_density from
+(select * from dishes where row_num <= 2);
