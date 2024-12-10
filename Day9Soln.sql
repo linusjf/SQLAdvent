e250bcb1655e9a71f8664c5329fcb479b1891fc9
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-10 12:42:04 +0530

    feat: update Day 9 solution

--- Day9Soln.sql
+++ Day9Soln.sql
@@ -35,5 +35,15 @@ WITH dishes AS (SELECT
   JOIN
     events ON menu.event_id = events.event_id
 )
-SELECT dish_name, event_name, calorie_density FROM
-(SELECT * FROM dishes WHERE row_num <= 3);
+SELECT dish_name, event_name, calorie_density FROM dishes WHERE row_num <= 3;
+WITH dishes AS (SELECT
+    dish_name,
+    event_name,
+    round(calories / weight_g,2) AS calorie_density,
+    ROW_NUMBER() OVER (PARTITION BY menu.event_id ORDER BY round(calories / weight_g,2) DESC) AS row_num
+  FROM
+    menu
+  JOIN
+    events using(event_id)
+)
+SELECT dish_name, event_name, calorie_density FROM dishes WHERE row_num <= 3;
