f01bb8c35262a236f541309d20d3e692116ed0ba
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-11 06:06:03 +0530

    Update SQL queries to use integer types for calories and weight.

--- Day9Soln.sql
+++ Day9Soln.sql
@@ -1,7 +1,7 @@
 SELECT
   dish_name,
   event_name,
-  round(calories / weight_g, 2) AS calorie_density
+  round(1.0 * calories / weight_g, 2) AS calorie_density
 FROM
   menu
 JOIN
@@ -16,8 +16,8 @@ FROM (
   SELECT
     dish_name,
     event_name,
-    round(calories / weight_g,2) AS calorie_density,
-    ROW_NUMBER() OVER (PARTITION BY menu.event_id ORDER BY round(calories / weight_g,2) DESC) AS row_num
+    round(1.0 * calories / weight_g,2) AS calorie_density,
+    ROW_NUMBER() OVER (PARTITION BY menu.event_id ORDER BY round(1.0 * calories / weight_g,2) DESC) AS row_num
   FROM
     menu
   JOIN
@@ -28,8 +28,8 @@ WHERE
 WITH dishes AS (SELECT
     dish_name,
     event_name,
-    round(calories / weight_g,2) AS calorie_density,
-    ROW_NUMBER() OVER (PARTITION BY menu.event_id ORDER BY round(calories / weight_g,2) DESC) AS row_num
+    round(CAST(calories AS float) / weight_g,2) AS calorie_density,
+    ROW_NUMBER() OVER (PARTITION BY menu.event_id ORDER BY round(CAST(calories AS float) / weight_g,2) DESC) AS row_num
   FROM
     menu
   JOIN
@@ -39,8 +39,8 @@ SELECT dish_name, event_name, calorie_density FROM dishes WHERE row_num <= 3;
 WITH dishes AS (SELECT
     dish_name,
     event_name,
-    round(calories / weight_g,2) AS calorie_density,
-    ROW_NUMBER() OVER (PARTITION BY menu.event_id ORDER BY round(calories / weight_g,2) DESC) AS row_num
+    round(CAST(calories AS float) / weight_g,2) AS calorie_density,
+    ROW_NUMBER() OVER (PARTITION BY menu.event_id ORDER BY round(CAST(calories AS float) / weight_g,2) DESC) AS row_num
   FROM
     menu
   JOIN
