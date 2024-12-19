3b427fc671ce6bc8158ec6fbaa70918d65926f32
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-19 05:31:16 +0530

    feat: add Day 17 solution.

---
+++ Day18Soln.sql
@@ -0,0 +1,30 @@
+WITH ActivityAverage AS (
+    SELECT
+        a.activity_id,
+        a.activity_name,
+        AVG(r.rating) AS average_rating
+    FROM
+        activities a
+    JOIN
+        activity_ratings r
+    ON
+        a.activity_id = r.activity_id
+    GROUP BY
+        a.activity_id, a.activity_name
+),
+RankedActivities AS (
+    SELECT
+        activity_id,
+        activity_name,
+        average_rating,
+        RANK() OVER (ORDER BY average_rating DESC) AS rank
+    FROM
+        ActivityAverage
+)
+SELECT
+    activity_name,
+    average_rating
+FROM
+    RankedActivities
+WHERE
+    rank <= 2;
