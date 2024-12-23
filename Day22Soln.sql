de1af8093218b5ad41557cdc2b625f5f11d63315
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-23 05:50:13 +0530

    feat: add alternate solutions

--- Day22Soln.sql
+++ Day22Soln.sql
@@ -21,3 +21,12 @@ SELECT
 FROM
   guests
   JOIN guest_gifts USING (guest_id);
+
+SELECT g.guest_name
+FROM guests g
+LEFT JOIN guest_gifts gg ON g.guest_id = gg.guest_id
+WHERE gg.gift_id IS NULL;
+
+SELECT guest_name
+FROM guests g
+WHERE NOT EXISTS (SELECT 1 FROM guest_gifts gg WHERE gg.guest_id = g.guest_id);
