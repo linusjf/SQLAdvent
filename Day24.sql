b15603de00b58979d2fcb5812ca5e17ed2721287
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-25 10:49:42 +0530

    fix: format

--- Day24.sql
+++ Day24.sql
@@ -6,8 +6,10 @@ CREATE TABLE deliveries (
   delivery_date DATE NOT NULL,
   gifts_delivered INTEGER NOT NULL
 );
+
 -- Insert data
-INSERT INTO deliveries (delivery_date, gifts_delivered)
+INSERT INTO
+  deliveries (delivery_date, gifts_delivered)
 VALUES
   ('2024-12-20', 120),
   ('2024-12-21', 150),
--- Day24Soln.sql
+++ Day24Soln.sql
@@ -1,10 +1,13 @@
 SELECT
-    delivery_date,
-    SUM(gifts_delivered) OVER (
-        ORDER BY delivery_date -- Window ordering (not necessarily the same as result ordering!)
-        ROWS BETWEEN -- Window for the SUM includes these rows:
-            UNBOUNDED PRECEDING -- all rows before current one in window ordering
-            AND CURRENT ROW -- up to and including current row.
-        ) AS cumulative_sum
-FROM deliveries
-ORDER BY delivery_date;
+  delivery_date,
+  SUM(gifts_delivered) OVER (
+    ORDER BY
+      delivery_date -- Window ordering (not necessarily the same as result ordering!)
+      ROWS BETWEEN -- Window for the SUM includes these rows:
+      UNBOUNDED PRECEDING -- all rows before current one in window ordering
+      AND CURRENT ROW -- up to and including current row.
+  ) AS cumulative_sum
+FROM
+  deliveries
+ORDER BY
+  delivery_date;
